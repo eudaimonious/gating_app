cbec5f70bfca156775c24eed9f3edb4b7cfd966a
Margaret Scott <eudaimonious@gmail.com>
2013-08-03 17:29:03 -0400

    Install devise and create User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ GatingApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #As requested by Devise during its install
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
