4a11868ae63294704feaf9b09176ac491a115b04
Margaret Scott <eudaimonious@gmail.com>
2013-08-03 15:44:58 -0400

    Rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GatingApp::Application.config.secret_token = 'f5b5961e5407520e6e676fcadbbf9687c857cb8ba98b151da912f084b965188974a9c19cf084f53ce10e231707e3ecc0263b8c963781a2da1aa11729bffeb095'
