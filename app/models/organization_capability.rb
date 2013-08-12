c00bcb0d6256e3afa817a921a2c0faf810c11ba7
Margaret Scott <eudaimonious@gmail.com>
2013-08-12 13:48:34 -0400

    Clean up migrations and renamed model

--- app/models/organization_capability.rb
+++
@@ -1,4 +0,0 @@
-class OrganizationCapability < ActiveRecord::Base
-	belongs_to :organization
-	belongs_to :capability
- end
--- db/migrate/20130811230532_remove_val_type_from_capabilities.rb
+++
@@ -1,7 +0,0 @@
-class RemoveValTypeFromCapabilities < ActiveRecord::Migration
-  def change
-  	change_table :capabilities do |t|
-      t.remove :val_type
-     end
-  end
-end
