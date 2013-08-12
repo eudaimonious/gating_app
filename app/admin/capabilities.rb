23c35581dbbf78ed332a6093c669c02671d0413e
Margaret Scott <eudaimonious@gmail.com>
2013-08-11 22:16:57 -0400

    Select capability owner

--- app/admin/capabilities.rb
+++ app/admin/capabilities.rb
@@ -3,6 +3,8 @@ ActiveAdmin.register Capability do
 	form do |f|
 		f.inputs "Capability" do
 			f.input :name
+			#Select an admin user as the owner
+			f.input :user_id, :as => :select, :collection => options_for_select(User.where(admin: true).map{ |u| [u.email, u.id]}), :label => "Capability Owner"
 			f.input :key, :label => "Key, for API access"
 			f.input :expiry_date, :label => "Optional expiry date"
 		end
--- app/models/capability.rb
+++ app/models/capability.rb
@@ -1,6 +1,6 @@
 class Capability < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :expiry_date, :key, :name
+  attr_accessible :expiry_date, :key, :name, :user_id
   has_many :organizations_capabilities
   has_many :organizations, :through => :organizations_capabilities
  end
